7298df71d3ddc86a83966bf1576a7ef28ec643f1
NazmiDere <nazmidere48@gmail.com>
2019-10-01 09:42:43 +0200

    Assignment-NazmiDere

--- Sherlock and Squares.py
+++ Sherlock and Squares.py
@@ -1,5 +1,5 @@
 q = int(input())
 for q_itr in range(q):
-    a, b = list(map(int,input().split()))
-    lst = [i for i in range(a, b + 1) if (i ** 0.5).is_integer()]
-    print(len(lst))
+    a, b = list(map(int, input().split()))
+    result = int(b ** (1 / 2)) - int((a - 1) ** (1 / 2))
+    print(result)
